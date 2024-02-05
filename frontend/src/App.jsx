097e519fa92244e428b0d358d9682cf0ab37df71
Khairat Adesina <khairatadesina01@gmail.com>
2024-02-05 20:44:50 +0100

    add: implemented the Restuarant home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,19 +10,25 @@ import RestuarantHeroPage from './components/RestuarantHero';
 import RiderSignIn from './components/RiderSignIn';
 import RestaurantLogin from './components/RestaurantLogin';
 import RiderHeroPage from './components/RiderHero';
+import RestuarantSideBar from './components/RestuarantSideBar';
+import RestuarantHome from './components/RestuarantHome';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+
         <Route path="/auth/sign_up_1" element={<UserSignUp />} />
         <Route path="/auth/sign_up_2" element={<UserSignUp2 />} />
         <Route path="/auth/success" element={<UserHeroPage />} />
         <Route path="/auth/login" element={<UserLogin />} />
+
         <Route path="/auth/restuarant/sign_up" element={<ResturantSignUp />} />
         <Route path="/auth/restuarant/login" element={<RestaurantLogin />} />
         <Route path="/auth/restuarant/success" element={< RestuarantHeroPage />} />
+        <Route path="/restuarant/home" element={<RestuarantHome />} />
+
         <Route path="/auth/rider/sign_up" element={<RiderSignIn />} />
         <Route path="/auth/rider/success" element={<RiderHeroPage />} />
         {/* Add more routes as needed */}
---
+++ frontend/src/components/RestuarantHome.jsx
@@ -0,0 +1,16 @@
+// This file displays the home page of the restuarant route
+
+import RestuarantSideBar from "./RestuarantSideBar"
+
+function RestuarantHome() {
+  return (
+    <div className="flex flex-row bg-powder-blue w-full h-screen">
+        <RestuarantSideBar />
+        <div className=" ml-72 px-5 bg-powder-blue py-5 ">
+                HAPPY
+        </div>
+    </div>
+  )
+}
+
+export default RestuarantHome
