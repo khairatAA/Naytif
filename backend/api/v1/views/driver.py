6379063c45290d2c484199620d2ff43027403751
Omar Jammeh <orms45@gmail.com>
2024-02-10 15:20:37 +0000

    changes to response codes in the drivers route

--- backend/api/v1/views/driver.py
+++ backend/api/v1/views/driver.py
@@ -22,38 +22,24 @@ def create_driver():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     email = request.form['email']
-    password = request.form['password']
     phone = request.form['phone']
     vehicle_type = request.form['vehicle_type']
     driver = db.session.execute(db.select(Driver).where(Driver.email == email)).scalar()
     if driver:
-        return jsonify({'error': 'driver already exist'})
-    if first_name and last_name and email and password:
+        return jsonify(msg='Driver already exist'), 400
+    if first_name and last_name and email:
         new_driver = Driver(
             id=str(uuid.uuid4()),
             first_name=first_name,
             last_name=last_name,
             email=email,
-            password=password,
             phone=phone,
             vehicle_type=vehicle_type
         )
         db.session.add(new_driver)
         db.session.commit()
-        driver_dict = {
-            'id': new_driver.id,
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'password': password,
-            'phone': phone,
-            'image_url': new_driver.image_url,
-            'created_at': new_driver.created_at,
-            'updated_at': new_driver.updated_at,
-            'vehicle_type': new_driver.vehicle_type
-        }
-        return jsonify(driver=driver_dict)
-    return jsonify(error={"error": "invalid entry"})
+        return jsonify(msg="Driver successfully created."), 201
+    return jsonify(msg="Invalid entry"), 400
 
 
 @app.route('/drivers/<driver_id>', methods=['GET', 'PATCH', 'DELETE'])
@@ -63,12 +49,12 @@ def driver_by_id(driver_id):
         driver = db.get_or_404(Driver, driver_id)
     except Exception:
         return jsonify({'error': 'driver not found'})
-    
+
     # get driver information
     if request.method == 'GET':
         driver_dict = driver.to_dict()
         return jsonify(driver=driver_dict)
-    
+
     # update driver information
     if request.method == 'PATCH':
         if request.form.get('phone'):
@@ -80,7 +66,7 @@ def driver_by_id(driver_id):
         driver.updated_at = datetime.datetime.now()
         db.session.commit()
         return jsonify({"Success": "Successfully updated the driver."})
-    
+
     # delete driver
     db.session.delete(driver)
     db.session.commit()
