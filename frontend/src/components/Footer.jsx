8f9a0f2226f80c04e215c98b22242577164799d6
Khairat Adesina <khairatadesina01@gmail.com>
2024-02-21 03:20:52 +0100

    fix: changed footer content

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,8 @@ import { Copyright } from 'lucide-react';
 import twitter from '../assets/twitter.svg';
 import whatsapp from '../assets/whatsapp.svg';
 import facebook from '../assets/facebook.svg';
+import { Link } from "react-router-dom";
+import { Linkedin } from 'lucide-react';
 
 function Footer() {
     return (
@@ -13,26 +15,29 @@ function Footer() {
             </div>
             <div className=' flex flex-row justify-between gap-20'>
                 <div className=' flex flex-col gap-1'>
-                    <a href="">Get help</a>
-                    <a href="">Sign Up</a>
-                    <a href="">Log in</a>
-                    <a href="">Create a business account</a>
-                    <a href="">Promotions</a>
+                    <Link to="">Get help</Link>
+                    <Link to="">Sign Up</Link>
+                    <Link to="">Log in</Link>
+                    <Link to="">Create a business account</Link>
+                    <Link to="">Promotions</Link>
                 </div>
                 <div className=' flex flex-col gap-1'>
-                    <a href="">Restaurants Near Me</a>
-                    <a href="">Menu Categories</a>
-                    <a href="">View all Cities</a>
-                    <a href="">Pick Up or Delivery</a>
-                    <a href="">About Naytiv</a>
+                    <Link to="">Restaurants Near Me</Link>
+                    <Link to="">Menu Categories</Link>
+                    <Link to="">View all Cities</Link>
+                    <Link to="">Pick Up or Delivery</Link>
+                    <Link to="">About Naytiv</Link>
                 </div>
             </div>
         </div>
         <div className=' flex flex-col items-center justify-center gap-3'>
-            <div className=' flex flex-row gap-3'>
-                <a href=""><img src={twitter} alt="" /></a>
-                <a href=""><img src={whatsapp} alt="" /></a>
-                <a href=""><img src={facebook} alt="" /></a>
+            <div className=' flex flex-row gap-3 items-baseline'>
+                <p className=' font-semibold'>Built by Khairat Adesina</p>
+                <a href="https://www.linkedin.com/in/khairat-adesina1234/" target='_blank'><Linkedin size={20} color='blue' /></a>
+                <a href="https://twitter.com/_dedamola" target='_blank'><img src={twitter} className=' w-5' alt="" /></a>
+                <p className=' font-semibold'> and Omar Jammeh</p>
+                <a href="http://www.linkedin.com/in/omar-jammeh-a4b0b029a" target='_blank'><Linkedin size={20} color='blue' /></a>
+                <a href="https://twitter.com" target='_blank'><img src={twitter} className=' w-5' alt="" /></a>
             </div>
             <div className=' flex flex-row justify-center items-center gap-3'>
                 <Copyright />
