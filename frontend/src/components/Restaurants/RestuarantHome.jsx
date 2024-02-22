6ad38ed5d57de7d3e6e01235db32c0b81fb38cba
Khairat Adesina <khairatadesina01@gmail.com>
2024-02-22 10:45:30 +0100

    add: added a post request to post the user orders to the backend

--- frontend/src/components/Restaurants/RestuarantHome.jsx
+++ frontend/src/components/Restaurants/RestuarantHome.jsx
@@ -8,6 +8,7 @@ function RestuarantHome() {
 
   const [restaurants, setRestaurants] = useState([]);
   const restaurant_id = localStorage.getItem("restaurant_id");
+  const [restaurantOrders, setRestaurantOrders] = useState([]);
 
   useEffect(() => {
       const fetchRestaurants = async () => {
@@ -26,14 +27,31 @@ function RestuarantHome() {
       fetchRestaurants(); // Don't forget to invoke the function
   }, []);
 
+  useEffect(() => {
+    const fetchRestaurantOrders = async () => {
+      try {
+        // Fetch orders specific to the restaurant
+        const response = await api.get(`/restaurants/${restaurant_id}/orders`);
+        const orders = response.data.orders;
+        setRestaurantOrders(orders);
+      } catch (error) {
+        console.error('Error fetching restaurant orders:', error);
+      }
+    };
+
+    fetchRestaurantOrders(); // Fetch orders when component mounts
+    // You can also establish a real-time connection here to receive new order notifications
+  }, []);
+
+
   return (
     <div className="flex flex-row bg-powder-blue w-full h-screen">
         <RestuarantSideBar />
         <div className=" ml-72 max-md:ml-16 px-5 bg-powder-blue w-full py-5 flex flex-col gap-14 ">
             <RestaurantTop />
             <h1 className=' font-extrabold text-3xl'>Welcome {restaurants.brand_name} !</h1>
-            <div className=' '>
-            
+            <div className=''>
+                
             </div>     
         </div>
     </div>
